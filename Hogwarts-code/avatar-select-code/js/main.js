901e07fe1a261b1db08f5c8e15ca4ac31afcdb31
Ashlay <105703068+AshlayMeijer@users.noreply.github.com>
2023-04-18 14:52:49 +0200

    Fix a lot of mistakes in code, split into more then one file

--- Hogwarts-code/avatar-select-code/js/main.js
+++ Hogwarts-code/avatar-select-code/js/main.js
@@ -20,25 +20,38 @@ class Header {
     }
 }
 
+class RandomNumber {
+    array;
+
+    constructor(array) {
+        this.array = array;
+
+        this.i = this.array.length;
+        this.j = 0;
+        this.temp = [];
+        while (this.i--) {
+            this.j = Math.floor(Math.random() * (this.i + 1));
+            this.temp = this.array[this.i];
+            this.array[this.i] = this.array[this.j];
+            this.array[this.j] = this.temp;
+        }
+    }
+}
+
 class AvatarCirlce {
     placeToRender;
     liElement;
     buttonElement;
     imgElement;
-    number;
+    id;
     alt;
     src;
+    number;
 
-    constructor(placeToRender, number) {
-        this.placeToRender = placeToRender;
+    constructor(placeToRender, id, number) {
         this.number = number;
-        if (this.number > 6) {
-            this.alt = 'random';
-            this.src = `./img/question.webp`
-        } else {
-            this.alt = this.number;
-            this.src = `./img/av-${this.number}.webp`;
-        }
+        this.placeToRender = placeToRender;
+        this.id = id;
 
         this.liElement = document.createElement('li');
         this.buttonElement = document.createElement('button');
@@ -46,6 +59,15 @@ class AvatarCirlce {
 
         this.liElement.classList = 'circle';
         this.buttonElement.classList = 'img-buttons';
+
+        if (this.id > 6) {
+            this.alt = 'random';
+            this.src = `./img/question.webp`;
+            this.liElement.classList = 'random__circle-1';
+        } else {
+            this.alt = this.id;
+            this.src = `./img/av-${this.number}.webp`;
+        }
 
         this.imgElement.src = this.src;;
         this.imgElement.alt = `avatar-${this.alt}`;
@@ -96,15 +118,19 @@ class Button {
         this.placeToRender = placeToRender;
 
         this.buttonElement = document.createElement('button');
+        this.buttonWrapper = document.createElement('div');
 
         this.buttonElement.classList = 'Proceed__Button';
         this.buttonElement.textContent = 'Proceed';
+
+        this.buttonWrapper.classList = 'button__Wrapper';
 
         this.render();
     }
 
     render = () => {
-        this.placeToRender.appendChild(this.buttonElement);
+        this.placeToRender.appendChild(this.buttonWrapper);
+        this.buttonWrapper.appendChild(this.buttonElement);
     }
 }
 
@@ -113,6 +139,7 @@ class App {
     header;
     avatars;
     avatarCard;
+    RandomNumber;
 
     constructor(placeToRender) {
         this.placeToRender = document.getElementsByTagName(placeToRender)[0];
@@ -120,14 +147,15 @@ class App {
         this.header = new Header(this.placeToRender);
         this.avatars = new Avatars(this.placeToRender);
         this.button = new Button(this.placeToRender);
+        this.RandomNumber = new RandomNumber([1, 2, 3, 4, 5, 6]);
 
-        this.generateAvatars();
+        this.generateAvatars(this.RandomNumber.array);
     }
 
-    generateAvatars = () => {
+    generateAvatars = (numArray) => {
         for (let i = 1; i < 8; i++) {
             if (i != 7) {
-                this.avatarCard = new AvatarCirlce(this.avatars.listElement, i);
+                this.avatarCard = new AvatarCirlce(this.avatars.listElement, i, numArray[i - 1]);
             } else this.avatarCard = new AvatarCirlce(this.avatars.randomListElement, i);
         }
     }
